e9451881c2409173ee527ddef0a4f49af2566493
hyrorre <hiromu.yamashita@hotmail.com>
2024-09-11 05:09:39 +0900

    Update: prettierrc and run prettier

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,247 +1,244 @@
 <script setup>
-import { ref } from 'vue';
-import { Link, router, useForm } from '@inertiajs/vue3';
-import ActionMessage from '@/Components/ActionMessage.vue';
-import FormSection from '@/Components/FormSection.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import SecondaryButton from '@/Components/SecondaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import { ref } from 'vue'
+import { Link, router, useForm } from '@inertiajs/vue3'
+import ActionMessage from '@/Components/ActionMessage.vue'
+import FormSection from '@/Components/FormSection.vue'
+import InputError from '@/Components/InputError.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
+import SecondaryButton from '@/Components/SecondaryButton.vue'
+import TextInput from '@/Components/TextInput.vue'
 
 const props = defineProps({
-    user: Object,
-});
+  user: Object
+})
 
 const form = useForm({
-    _method: 'PUT',
-    name: props.user.name,
-    email: props.user.email,
-    photo: null,
-    iidxid: props.user.iidxid,
-    infinitasid: props.user.infinitasid,
-    apikey: props.user.apikey,
-    scope: props.user.scope,
-});
+  _method: 'PUT',
+  name: props.user.name,
+  email: props.user.email,
+  photo: null,
+  iidxid: props.user.iidxid,
+  infinitasid: props.user.infinitasid,
+  apikey: props.user.apikey,
+  scope: props.user.scope
+})
 
-const verificationLinkSent = ref(null);
-const photoPreview = ref(null);
-const photoInput = ref(null);
+const verificationLinkSent = ref(null)
+const photoPreview = ref(null)
+const photoInput = ref(null)
 
 const updateProfileInformation = () => {
-    if (photoInput.value) {
-        form.photo = photoInput.value.files[0];
-    }
+  if (photoInput.value) {
+    form.photo = photoInput.value.files[0]
+  }
 
-    form.post(route('user-profile-information.update'), {
-        errorBag: 'updateProfileInformation',
-        preserveScroll: true,
-        onSuccess: () => clearPhotoFileInput(),
-    });
-};
+  form.post(route('user-profile-information.update'), {
+    errorBag: 'updateProfileInformation',
+    preserveScroll: true,
+    onSuccess: () => clearPhotoFileInput()
+  })
+}
 
 const sendEmailVerification = () => {
-    verificationLinkSent.value = true;
-};
+  verificationLinkSent.value = true
+}
 
 const selectNewPhoto = () => {
-    photoInput.value.click();
-};
+  photoInput.value.click()
+}
 
 const updatePhotoPreview = () => {
-    const photo = photoInput.value.files[0];
+  const photo = photoInput.value.files[0]
 
-    if (!photo) return;
+  if (!photo) return
 
-    const reader = new FileReader();
+  const reader = new FileReader()
 
-    reader.onload = (e) => {
-        photoPreview.value = e.target.result;
-    };
+  reader.onload = (e) => {
+    photoPreview.value = e.target.result
+  }
 
-    reader.readAsDataURL(photo);
-};
+  reader.readAsDataURL(photo)
+}
 
 const deletePhoto = () => {
-    router.delete(route('current-user-photo.destroy'), {
-        preserveScroll: true,
-        onSuccess: () => {
-            photoPreview.value = null;
-            clearPhotoFileInput();
-        },
-    });
-};
+  router.delete(route('current-user-photo.destroy'), {
+    preserveScroll: true,
+    onSuccess: () => {
+      photoPreview.value = null
+      clearPhotoFileInput()
+    }
+  })
+}
 
 const clearPhotoFileInput = () => {
-    if (photoInput.value?.value) {
-        photoInput.value.value = null;
-    }
-};
+  if (photoInput.value?.value) {
+    photoInput.value.value = null
+  }
+}
 
-const apikeyCopied = ref(false);
-const apikeyInputType = ref('password');
+const apikeyCopied = ref(false)
+const apikeyInputType = ref('password')
 
 const copyApikey = () => {
-    apikeyInputType.value = 'text';
-    navigator.clipboard.writeText(form.apikey).then(() => {
-        apikeyCopied.value = true;
-    });
-};
+  apikeyInputType.value = 'text'
+  navigator.clipboard.writeText(form.apikey).then(() => {
+    apikeyCopied.value = true
+  })
+}
 
 const generateApikey = () => {
-    form.apikey = crypto.randomUUID();
-    copyApikey();
-};
+  form.apikey = crypto.randomUUID()
+  copyApikey()
+}
 </script>
 
 <template>
-    <FormSection @submitted="updateProfileInformation">
-        <template #title> Profile Information </template>
+  <FormSection @submitted="updateProfileInformation">
+    <template #title> Profile Information </template>
 
-        <template #description> Update your account's profile information and email address. </template>
+    <template #description> Update your account's profile information and email address. </template>
 
-        <template #form>
-            <!-- Profile Photo -->
-            <div v-if="$page.props.jetstream.managesProfilePhotos" class="col-span-6 sm:col-span-4">
-                <!-- Profile Photo File Input -->
-                <input id="photo" ref="photoInput" type="file" class="hidden" @change="updatePhotoPreview" />
+    <template #form>
+      <!-- Profile Photo -->
+      <div v-if="$page.props.jetstream.managesProfilePhotos" class="col-span-6 sm:col-span-4">
+        <!-- Profile Photo File Input -->
+        <input id="photo" ref="photoInput" type="file" class="hidden" @change="updatePhotoPreview" />
 
-                <InputLabel for="photo" value="Photo" />
+        <InputLabel for="photo" value="Photo" />
 
-                <!-- Current Profile Photo -->
-                <div v-show="!photoPreview" class="mt-2">
-                    <img :src="user.profile_photo_url" :alt="user.name" class="rounded-full h-20 w-20 object-cover" />
-                </div>
+        <!-- Current Profile Photo -->
+        <div v-show="!photoPreview" class="mt-2">
+          <img :src="user.profile_photo_url" :alt="user.name" class="rounded-full h-20 w-20 object-cover" />
+        </div>
 
-                <!-- New Profile Photo Preview -->
-                <div v-show="photoPreview" class="mt-2">
-                    <span
-                        class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                        :style="'background-image: url(\'' + photoPreview + '\');'"
-                    />
-                </div>
+        <!-- New Profile Photo Preview -->
+        <div v-show="photoPreview" class="mt-2">
+          <span
+            class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
+            :style="'background-image: url(\'' + photoPreview + '\');'"
+          />
+        </div>
 
-                <SecondaryButton class="mt-2 me-2" type="button" @click.prevent="selectNewPhoto">
-                    Select A New Photo
-                </SecondaryButton>
+        <SecondaryButton class="mt-2 me-2" type="button" @click.prevent="selectNewPhoto">
+          Select A New Photo
+        </SecondaryButton>
 
-                <SecondaryButton v-if="user.profile_photo_path" type="button" class="mt-2" @click.prevent="deletePhoto">
-                    Remove Photo
-                </SecondaryButton>
+        <SecondaryButton v-if="user.profile_photo_path" type="button" class="mt-2" @click.prevent="deletePhoto">
+          Remove Photo
+        </SecondaryButton>
 
-                <InputError :message="form.errors.photo" class="mt-2" />
-            </div>
+        <InputError :message="form.errors.photo" class="mt-2" />
+      </div>
 
-            <!-- Name -->
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="name" value="ID" />
-                <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" required />
-                <InputError :message="form.errors.name" class="mt-2" />
-            </div>
+      <!-- Name -->
+      <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="name" value="ID" />
+        <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" required />
+        <InputError :message="form.errors.name" class="mt-2" />
+      </div>
 
-            <!-- Email -->
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="username"
-                />
-                <InputError :message="form.errors.email" class="mt-2" />
+      <!-- Email -->
+      <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="email" value="Email" />
+        <TextInput
+          id="email"
+          v-model="form.email"
+          type="email"
+          class="mt-1 block w-full"
+          required
+          autocomplete="username"
+        />
+        <InputError :message="form.errors.email" class="mt-2" />
 
-                <div v-if="$page.props.jetstream.hasEmailVerification && user.email_verified_at === null">
-                    <p class="text-sm mt-2 dark:text-white">
-                        Your email address is unverified.
+        <div v-if="$page.props.jetstream.hasEmailVerification && user.email_verified_at === null">
+          <p class="text-sm mt-2 dark:text-white">
+            Your email address is unverified.
 
-                        <Link
-                            :href="route('verification.send')"
-                            method="post"
-                            as="button"
-                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                            @click.prevent="sendEmailVerification"
-                        >
-                            Click here to re-send the verification email.
-                        </Link>
-                    </p>
+            <Link
+              :href="route('verification.send')"
+              method="post"
+              as="button"
+              class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+              @click.prevent="sendEmailVerification"
+            >
+              Click here to re-send the verification email.
+            </Link>
+          </p>
 
-                    <div
-                        v-show="verificationLinkSent"
-                        class="mt-2 font-medium text-sm text-green-600 dark:text-green-400"
-                    >
-                        A new verification link has been sent to your email address.
-                    </div>
-                </div>
-            </div>
+          <div v-show="verificationLinkSent" class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
+            A new verification link has been sent to your email address.
+          </div>
+        </div>
+      </div>
 
-            <!-- IIDX ID -->
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="iidxid" value="IIDX ID" />
-                <TextInput id="iidxid" v-model="form.iidxid" type="text" class="mt-1 block w-full" />
-                <InputError :message="form.errors.iidxid" class="mt-2" />
-            </div>
+      <!-- IIDX ID -->
+      <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="iidxid" value="IIDX ID" />
+        <TextInput id="iidxid" v-model="form.iidxid" type="text" class="mt-1 block w-full" />
+        <InputError :message="form.errors.iidxid" class="mt-2" />
+      </div>
 
-            <!-- INFINITAS ID -->
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="infinitasid" value="INFINITAS ID" />
-                <TextInput id="infinitasid" v-model="form.infinitasid" type="text" class="mt-1 block w-full" />
-                <InputError :message="form.errors.infinitasid" class="mt-2" />
-            </div>
+      <!-- INFINITAS ID -->
+      <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="infinitasid" value="INFINITAS ID" />
+        <TextInput id="infinitasid" v-model="form.infinitasid" type="text" class="mt-1 block w-full" />
+        <InputError :message="form.errors.infinitasid" class="mt-2" />
+      </div>
 
-            <!-- API Key -->
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="apikey" value="API Key" />
-                <TextInput
-                    id="apikey"
-                    v-model="form.apikey"
-                    :type="apikeyInputType"
-                    class="mt-1 block w-full"
-                    required
-                    disabled
-                />
-                <InputError :message="form.errors.apikey" class="mt-2" />
-                <div class="flex justify-end items-center align-middle">
-                    <ActionMessage :on="apikeyCopied" class="mt-2 me-3">Copied.</ActionMessage>
-                    <SecondaryButton class="mt-2 me-2" @click="copyApikey">Copy</SecondaryButton>
-                    <SecondaryButton class="mt-2" @click="generateApikey">Regenerate</SecondaryButton>
-                </div>
-            </div>
+      <!-- API Key -->
+      <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="apikey" value="API Key" />
+        <TextInput
+          id="apikey"
+          v-model="form.apikey"
+          :type="apikeyInputType"
+          class="mt-1 block w-full"
+          required
+          disabled
+        />
+        <InputError :message="form.errors.apikey" class="mt-2" />
+        <div class="flex justify-end items-center align-middle">
+          <ActionMessage :on="apikeyCopied" class="mt-2 me-3">Copied.</ActionMessage>
+          <SecondaryButton class="mt-2 me-2" @click="copyApikey">Copy</SecondaryButton>
+          <SecondaryButton class="mt-2" @click="generateApikey">Regenerate</SecondaryButton>
+        </div>
+      </div>
 
-            <!-- Scope -->
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel value="Scope" class="mb-1" />
+      <!-- Scope -->
+      <div class="col-span-6 sm:col-span-4">
+        <InputLabel value="Scope" class="mb-1" />
 
-                <label>
-                    <input
-                        type="radio"
-                        name="scope"
-                        v-model="form.scope"
-                        value="public"
-                        class="rounded-full dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
-                    />
-                    <span class="mx-2 text-sm text-gray-600 dark:text-gray-400">Public</span>
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="scope"
-                        v-model="form.scope"
-                        value="private"
-                        class="rounded-full dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
-                    />
-                    <span class="mx-2 text-sm text-gray-600 dark:text-gray-400">Private</span>
-                </label>
+        <label>
+          <input
+            type="radio"
+            name="scope"
+            v-model="form.scope"
+            value="public"
+            class="rounded-full dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+          />
+          <span class="mx-2 text-sm text-gray-600 dark:text-gray-400">Public</span>
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="scope"
+            v-model="form.scope"
+            value="private"
+            class="rounded-full dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+          />
+          <span class="mx-2 text-sm text-gray-600 dark:text-gray-400">Private</span>
+        </label>
 
-                <InputError :message="form.errors.scope" class="mt-2" />
-            </div>
-        </template>
+        <InputError :message="form.errors.scope" class="mt-2" />
+      </div>
+    </template>
 
-        <template #actions>
-            <ActionMessage :on="form.recentlySuccessful" class="me-3"> Saved. </ActionMessage>
+    <template #actions>
+      <ActionMessage :on="form.recentlySuccessful" class="me-3"> Saved. </ActionMessage>
 
-            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Save </PrimaryButton>
-        </template>
-    </FormSection>
+      <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Save </PrimaryButton>
+    </template>
+  </FormSection>
 </template>
